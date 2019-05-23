bd56c6cc14315e6ee1674bba23585051adcbced4
Manuel De la Penna <manueldelapenna@gmail.com>
2019-05-23 13:55:49 -0300

    arreglo carga de form con misma UA y subo version

--- src/client/meta-enc.ts
+++ src/client/meta-enc.ts
@@ -330,7 +330,7 @@ myOwn.wScreens.loadForm=async function(addrParams){
     var navigationStack:formStructure.NavigationStack[] = []
     for(var i=urlNavigationStack.length-1;  i>=0; i--){
         if(urlNavigationStack[i].formId != result.formulario){
-            formData = formData[urlNavigationStack[i].analysisUnit][urlNavigationStack[i+1].iPosition-1];
+            formData = formData[unidadAnalisis]?formData[urlNavigationStack[i].analysisUnit][urlNavigationStack[i+1].iPosition-1]:formData;
         }
         var stackElement: formStructure.NavigationStack = urlNavigationStack[i];
         stackElement.formData = formData;
