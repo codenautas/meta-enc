0af619fb1c4632c3f6b7e756bbfc15c4c7f926af
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2019-09-06 15:24:02 -0300

    adding conditions to update only empty var names, and only with non empty casillero's var names

--- install/update_var_names.sql
+++ install/update_var_names.sql
@@ -6,7 +6,7 @@ set role to eder2019_owner;
 update variables v
 set nombre = c.nombre
 from casilleros c
-where v.variable=c.var_name and v.clase = 'interna'
+where v.variable=c.var_name and v.clase = 'interna' and v.nombre is null and c.nombre is not null
 
 -- chequeo
 --    select v.variable, v.nombre, c.nombre, c.var_name 
