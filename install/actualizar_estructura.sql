2293b6596e55a3f34860e162a27097fa9b5f076f
Emilio Platzer <emilioplatzer@gmail.com>
2021-04-01 20:21:34 -0300

    Agrego leer, calculada, libre y especial en casilleros

---
+++ install/actualizar_estructura.sql
@@ -0,0 +1,4 @@
+alter table casilleros add column leer boolean;
+alter table casilleros add column calculada boolean;
+alter table casilleros add column libre boolean;
+alter table casilleros add column especial jsonb;
