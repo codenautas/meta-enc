44a3f3a052e77f2a7c1ac65e8b557bb00bd7a463
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2021-04-08 13:37:14 -0300

    descarta las opciones duplicadas (hecho con Raquel)

--- install/complete_variables.sql
+++ install/complete_variables.sql
@@ -1,12 +1,13 @@
 
 insert into variables_opciones (operativo, tabla_datos, variable, opcion, nombre, orden)
-select vv.operativo, vv.tabla_datos, vv.variable, o.casillero::integer, o.nombre, o.orden
-    from casilleros o join casilleros v on o.padre=(case when v.tipoc='OM' then v.padre||'/'||v.casillero else v.casillero end) and v.var_name is not null and o.tipoc='O'
-     join variables vv on vv.operativo=v.operativo and v.var_name= variable -- tabla_datos ??? es lo que quiero determinar
-    order by vv.operativo, vv.tabla_datos, vv.variable, o.orden;
+select var.operativo, var.tabla_datos, var.variable, o.casillero::integer, string_agg(o.nombre,'/'), min(o.orden)
+    from casilleros o join casilleros p on o.operativo=p.operativo and o.padre=(case when p.tipoc='OM' then p.padre||'/'||p.casillero else p.casillero end) and p.var_name is not null and o.tipoc='O'
+      join variables var on var.operativo=p.operativo and p.var_name= variable -- tabla_datos ??? es lo que quiero determinar
+    group by 1,2,3,4 --descartar opciones duplicadas por disintos formularios
+    order by var.operativo, var.tabla_datos, var.variable;
 
 
 update variables v
 set nombre = c.nombre
 from casilleros c
-where v.variable=c.var_name and v.clase = 'interna'
+where v.variable=c.var_name and v.clase = 'interna';
